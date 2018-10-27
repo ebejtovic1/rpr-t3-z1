192b69dfb199912b31c147d7618299a549e7141e
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2018-10-28 00:41:05 +0200

    Dodana klasa Imenik bez implementiranih metoda

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public abstract class TelefonskiBroj {
+import java.util.HashMap;
+
+public abstract class TelefonskiBroj extends HashMap <String, TelefonskiBroj> {
     public abstract String ispisi();
     public abstract int hashCode();
 }
