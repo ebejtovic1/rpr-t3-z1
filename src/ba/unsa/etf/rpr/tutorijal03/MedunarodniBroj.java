997256e7917f290a69aff3284a01f6aa7fd1b21f
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2018-10-27 22:51:50 +0200

    Dodana klasa MedunarodniBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MedunarodniBroj extends TelefonskiBroj{
+    private String drzava;
+    private String broj;
+
+    public MedunarodniBroj(String drzava, String broj){
+        this.drzava=drzava;
+        this.broj=broj;
+    }
+
+    @Override
+    public int hashCode(){
+        return 0;
+    }
+
+    @Override
+    public String ispisi(){
+        return drzava + " " + broj;
+    }
+
+
+}
