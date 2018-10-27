f221be8653a206167396556488f88eaf9443240c
ebejtovic1 <ebejtovic1@etf.unsa.ba>
2018-10-27 22:44:55 +0200

    Dodana klasa MobilniBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MobilniBroj extends TelefonskiBroj{
+    private int mobilnaMreza;
+    private String broj;
+    public MobilniBroj(int mobilnaMreza, String broj){
+        this.mobilnaMreza=mobilnaMreza;
+        this.broj=broj;
+    }
+
+    @Override
+    public int hashCode(){
+        return mobilnaMreza;
+    }
+
+    @Override
+    public String ispisi(){
+        return "0" + mobilnaMreza + "/" + broj;
+    }
+}
